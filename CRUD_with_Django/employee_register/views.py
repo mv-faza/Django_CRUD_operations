97f3b19c01e67d126228fb0e2b0001932b8ca50b
mv-faza <fazli-musaev@mail.ru>
2021-09-08 04:44:01 -0700

    worked with employee list and created tables

--- CRUD_with_Django/employee_register/views.py
+++ CRUD_with_Django/employee_register/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import redirect, render
 from .forms import EmployeeForm
+from .models import employee
 # Create your views here.
 
 def employee_list(request):
-    return render(request,'employee_register/employee_list.html')
+    context = {'employee_list': employee.objects.all()}
+    return render(request, "employee_register/employee_list.html", context)
 
 def employee_form(request):
     if request.method == 'GET':
         form = EmployeeForm()
         return render(request,'employee_register/employee_form.html',{'form':form})
     else:
-        form = EmployeeForm()
+        form = EmployeeForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect('/employee/list')

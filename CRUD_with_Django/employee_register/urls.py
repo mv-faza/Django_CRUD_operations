033274480289f2f9a685a6785f16f5a6dcfefe93
mv-faza <fazli-musaev@mail.ru>
2021-09-14 04:24:07 -0700

    add for delete and update path

--- CRUD_with_Django/employee_register/urls.py
+++ CRUD_with_Django/employee_register/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.employee_form),
-    path('list/', views.employee_list),
+    path('', views.employee_form, name='employee_insert'),   # get and post
+    path('<int:id>/', views.employee_form,name='employee_update'),     #get and post
+    path('delete/<int:id>/',views.employee_delete,name='employee_delete' ),
+    path('list/', views.employee_list, name='employee_list'),      # get
 ]
